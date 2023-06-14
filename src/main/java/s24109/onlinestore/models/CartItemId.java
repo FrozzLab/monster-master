43398999a4e300b83bbf7f3f6c093cda10f328d4
FrozzLab <32843738+FrozzLab@users.noreply.github.com>
2023-06-14 02:32:43 +0200

    feat: improve visuals of index page

--- src/main/java/s24109/onlinestore/models/CartItemId.java
+++ src/main/java/s24109/onlinestore/models/CartItemId.java
@@ -1,10 +1,22 @@
 package s24109.onlinestore.models;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class CartItemId implements Serializable {
 
     private UserOrder userOrder;
     private Product product;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CartItemId that)) return false;
+        return Objects.equals(userOrder, that.userOrder) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userOrder, product);
+    }
 }
