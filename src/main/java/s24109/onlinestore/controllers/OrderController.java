a1d42211725f675e4249b21ac0bae385ba976b32
FrozzLab <32843738+FrozzLab@users.noreply.github.com>
2023-06-22 04:11:34 +0200

    feat: implement rough outline of cart page

--- src/main/java/s24109/onlinestore/controllers/OrderController.java
+++ src/main/java/s24109/onlinestore/controllers/OrderController.java
@@ -1,11 +1,27 @@
 package s24109.onlinestore.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import s24109.onlinestore.DAL.CartRepository;
 import s24109.onlinestore.DAL.OrderRepository;
 
 @Controller
 public class OrderController {
 
-    OrderRepository orderRepository;
+    private final OrderRepository orderRepository;
+    private final CartRepository cartRepository;
+
+    @Autowired
+    public OrderController(OrderRepository orderRepository, CartRepository cartRepository) {
+        this.orderRepository = orderRepository;
+        this.cartRepository = cartRepository;
+    }
+
+    @RequestMapping("/cart")
+    public String getProducts(Model model) {
+        return "cart";
+    }
 
 }
